6cd14b91f2f8c26b8aaa43ddceee2870016b04ac
Chua Kai Jun <chuakaijun.ckj@gmail.com>
2020-07-10 01:49:29 +0800

    Changed Onboarding to save values to SharedPref on clicking Next/Prev instead of RadioButton click.

--- app/src/main/java/com/nujiak/reconnaissance/onboarding/OnboardingFragments.kt
+++ app/src/main/java/com/nujiak/reconnaissance/onboarding/OnboardingFragments.kt
@@ -7,13 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.RadioButton
-import android.widget.RadioGroup
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.nujiak.reconnaissance.R
 import com.nujiak.reconnaissance.modalsheets.SettingsSheet
 
-class OnboardingTitleFragment(private val sharedPrefs: SharedPreferences, private val viewpager: ViewPager2): Fragment() {
+class OnboardingTitleFragment(
+    private val sharedPrefs: SharedPreferences,
+    private val viewpager: ViewPager2
+) : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -37,7 +39,10 @@ class OnboardingTitleFragment(private val sharedPrefs: SharedPreferences, privat
     }
 }
 
-class OnboardingGridsFragment(private val sharedPrefs: SharedPreferences, private val viewpager: ViewPager2): Fragment() {
+class OnboardingGridsFragment(
+    private val sharedPrefs: SharedPreferences,
+    private val viewpager: ViewPager2
+) : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -46,38 +51,50 @@ class OnboardingGridsFragment(private val sharedPrefs: SharedPreferences, privat
 
         val view = inflater.inflate(R.layout.onboarding_grids, container)
 
-        view.findViewById<Button>(R.id.onboarding_grids_next).setOnClickListener {
-            viewpager.currentItem += 1
-        }
-        view.findViewById<Button>(R.id.onboarding_grids_prev).setOnClickListener {
-            viewpager.currentItem -= 1
-        }
+        val utmBtn = view.findViewById<RadioButton>(R.id.onboarding_radio_utm)
+        val mgrsBtn = view.findViewById<RadioButton>(R.id.onboarding_radio_mgrs)
+        val kertauBtn = view.findViewById<RadioButton>(R.id.onboarding_radio_kertau)
 
         when (sharedPrefs.getInt(SettingsSheet.COORD_SYS_KEY, SettingsSheet.COORD_SYS_ID_UTM)) {
             SettingsSheet.COORD_SYS_ID_UTM ->
-                view.findViewById<RadioButton>(R.id.onboarding_radio_utm).isChecked = true
+                utmBtn.isChecked = true
             SettingsSheet.COORD_SYS_ID_MGRS ->
-                view.findViewById<RadioButton>(R.id.onboarding_radio_mgrs).isChecked = true
+                mgrsBtn.isChecked = true
             SettingsSheet.COORD_SYS_ID_KERTAU ->
-                view.findViewById<RadioButton>(R.id.onboarding_radio_kertau).isChecked = true
+                kertauBtn.isChecked = true
         }
 
-        val radioGroup = view.findViewById<RadioGroup>(R.id.onboarding_grids_radio_group)
-        radioGroup.setOnCheckedChangeListener { _, checkedId ->
-            val prefValue = when (checkedId) {
-                R.id.onboarding_radio_utm -> SettingsSheet.COORD_SYS_ID_UTM
-                R.id.onboarding_radio_mgrs -> SettingsSheet.COORD_SYS_ID_MGRS
-                R.id.onboarding_radio_kertau -> SettingsSheet.COORD_SYS_ID_KERTAU
-                else -> SettingsSheet.COORD_SYS_ID_UTM
-            }
-            sharedPrefs.edit().putInt(SettingsSheet.COORD_SYS_KEY, prefValue).apply()
+        view.findViewById<Button>(R.id.onboarding_grids_next).setOnClickListener {
+            sharedPrefs.edit().putInt(
+                SettingsSheet.COORD_SYS_KEY, when {
+                    utmBtn.isChecked -> SettingsSheet.COORD_SYS_ID_UTM
+                    mgrsBtn.isChecked -> SettingsSheet.COORD_SYS_ID_MGRS
+                    kertauBtn.isChecked -> SettingsSheet.COORD_SYS_ID_KERTAU
+                    else -> SettingsSheet.COORD_SYS_ID_UTM
+                }
+            ).apply()
+            viewpager.currentItem += 1
+        }
+        view.findViewById<Button>(R.id.onboarding_grids_prev).setOnClickListener {
+            sharedPrefs.edit().putInt(
+                SettingsSheet.COORD_SYS_KEY, when {
+                    utmBtn.isChecked -> SettingsSheet.COORD_SYS_ID_UTM
+                    mgrsBtn.isChecked -> SettingsSheet.COORD_SYS_ID_MGRS
+                    kertauBtn.isChecked -> SettingsSheet.COORD_SYS_ID_KERTAU
+                    else -> SettingsSheet.COORD_SYS_ID_UTM
+                }
+            ).apply()
+            viewpager.currentItem -= 1
         }
 
         return view
     }
 }
 
-class OnboardingAnglesFragment(private val sharedPrefs: SharedPreferences, private val viewpager: ViewPager2): Fragment() {
+class OnboardingAnglesFragment(
+    private val sharedPrefs: SharedPreferences,
+    private val viewpager: ViewPager2
+) : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -86,35 +103,41 @@ class OnboardingAnglesFragment(private val sharedPrefs: SharedPreferences, priva
 
         val view = inflater.inflate(R.layout.onboarding_angles, container)
 
+        val degBtn = view.findViewById<RadioButton>(R.id.onboarding_radio_deg)
+        val milsBtn = view.findViewById<RadioButton>(R.id.onboarding_radio_mils)
+
+        when (sharedPrefs.getInt(SettingsSheet.ANGLE_UNIT_KEY, SettingsSheet.ANGLE_UNIT_ID_DEG)) {
+            SettingsSheet.ANGLE_UNIT_ID_DEG -> degBtn.isChecked = true
+            SettingsSheet.ANGLE_UNIT_ID_NATO_MILS -> milsBtn.isChecked = true
+        }
+
         view.findViewById<Button>(R.id.onboarding_angles_next).setOnClickListener {
+            sharedPrefs.edit().putInt(
+                SettingsSheet.ANGLE_UNIT_KEY, when {
+                    degBtn.isChecked -> SettingsSheet.ANGLE_UNIT_ID_DEG
+                    milsBtn.isChecked -> SettingsSheet.ANGLE_UNIT_ID_NATO_MILS
+                    else -> SettingsSheet.ANGLE_UNIT_ID_DEG
+                }
+            ).apply()
             viewpager.currentItem += 1
         }
         view.findViewById<Button>(R.id.onboarding_angles_prev).setOnClickListener {
+            sharedPrefs.edit().putInt(
+                SettingsSheet.ANGLE_UNIT_KEY, when {
+                    degBtn.isChecked -> SettingsSheet.ANGLE_UNIT_ID_DEG
+                    milsBtn.isChecked -> SettingsSheet.ANGLE_UNIT_ID_NATO_MILS
+                    else -> SettingsSheet.ANGLE_UNIT_ID_DEG
+                }
+            ).apply()
             viewpager.currentItem -= 1
-        }
-
-        when (sharedPrefs.getInt(SettingsSheet.ANGLE_UNIT_KEY, SettingsSheet.ANGLE_UNIT_ID_DEG)) {
-            SettingsSheet.ANGLE_UNIT_ID_DEG ->
-                view.findViewById<RadioButton>(R.id.onboarding_radio_deg).isChecked = true
-            SettingsSheet.ANGLE_UNIT_ID_NATO_MILS ->
-                view.findViewById<RadioButton>(R.id.onboarding_radio_mils).isChecked = true
-        }
-
-        val radioGroup = view.findViewById<RadioGroup>(R.id.onboarding_angles_radio_group)
-        radioGroup.setOnCheckedChangeListener { _, checkedId ->
-            val prefValue = when (checkedId) {
-                R.id.onboarding_radio_deg -> SettingsSheet.ANGLE_UNIT_ID_DEG
-                R.id.onboarding_radio_mils -> SettingsSheet.ANGLE_UNIT_ID_NATO_MILS
-                else -> SettingsSheet.ANGLE_UNIT_ID_DEG
-            }
-            sharedPrefs.edit().putInt(SettingsSheet.ANGLE_UNIT_KEY, prefValue).apply()
         }
 
         return view
     }
 }
 
-class OnboardingEndFragment(private val viewpager: ViewPager2, val endOnClick: (View) -> Unit): Fragment() {
+class OnboardingEndFragment(private val viewpager: ViewPager2, val endOnClick: (View) -> Unit) :
+    Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -134,7 +157,8 @@ class OnboardingEndFragment(private val viewpager: ViewPager2, val endOnClick: (
     }
 }
 
-class OnboardingNSFragment(private val viewpager: ViewPager2, val endOnClick: (View) -> Unit): Fragment() {
+class OnboardingNSFragment(private val viewpager: ViewPager2, val endOnClick: (View) -> Unit) :
+    Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
