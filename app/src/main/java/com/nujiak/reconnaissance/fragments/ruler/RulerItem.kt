245401a1888ce1dbb6174d8d521e57f36013701c
Chua Kai Jun <chuakaijun.ckj@gmail.com>
2020-07-11 02:10:01 +0800

    Added Ruler calculation for Chains.

--- app/src/main/java/com/nujiak/reconnaissance/fragments/ruler/RulerItem.kt
+++ app/src/main/java/com/nujiak/reconnaissance/fragments/ruler/RulerItem.kt
@@ -1,21 +1,30 @@
 package com.nujiak.reconnaissance.fragments.ruler
 
-import com.nujiak.reconnaissance.database.Pin
+import com.google.android.gms.maps.model.LatLng
 
 sealed class RulerItem {
 
     companion object {
-        const val ITEM_ID_MEASUREMENT = -1L
-        const val ITEM_ID_EMPTY = -2L
+        const val ITEM_ID_MEASUREMENT = Long.MAX_VALUE
+        const val ITEM_ID_EMPTY = Long.MIN_VALUE
     }
 
     abstract val id: Long
 
-    data class RulerPinItem(val pin: Pin) : RulerItem() {
-        override val id = pin.pinId
+    data class RulerPointItem(
+        val adjustedId: Long,
+        val name: String,
+        val position: LatLng,
+        val colorId: Int
+    ) : RulerItem() {
+        override val id = adjustedId
     }
 
-    object RulerMeasurementItem : RulerItem() {
+    data class RulerMeasurementItem(
+        val points: List<LatLng>,
+        val startName: String,
+        val endName: String
+    ) : RulerItem() {
         override val id = ITEM_ID_MEASUREMENT
     }
 
