5b4a76cdac184438ca4815a37827cf25edb2d758
Chua Kai Jun <chuakaijun.ckj@gmail.com>
2021-12-28 22:55:24 +0800

    Remove unused import

--- app/src/main/java/com/nujiak/recce/fragments/GoToFragment.kt
+++ app/src/main/java/com/nujiak/recce/fragments/GoToFragment.kt
@@ -4,7 +4,6 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import com.google.android.gms.maps.model.LatLng
